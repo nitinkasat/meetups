a45aadc666ea8a84cebf2a2073b87be4e3febcd4
Nitin Kasat <nkasat@shutterfly.com>
2018-06-19 20:28:15 +0530

    Renaming project to insecure.

--- employee-management-insecure/src/test/java/com/emc/EmcApplicationTests.java
+++ employee-management-insecure/src/test/java/com/emc/EmcApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EmployeeManagementPortalApplicationTests {
+public class EmcApplicationTests {
 
 	@Test
 	public void contextLoads() {
